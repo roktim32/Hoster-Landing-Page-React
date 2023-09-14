73e85abac28099c307b3a7fbfee334fb53be3283
Roktim Kamal Senapoty <roktimsenapoty2@gmail.com>
2023-09-15 01:51:53 +0530

    Made tab and desktop responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,23 @@ const Navbar = () => {
                 <img src="./assets/Logo.svg" alt="" />
                 <button className='bg-gradient-to-r from-orange-400 to-red-400 text-white text-xs px-2.5 py-1.5 rounded-2xl'>Hoster is Hiring</button>
             </div>
-            <div className='flex justify-center items-center'>
+            <ul className='hidden lg:flex justify-between items-center font-lato text-gray-400 gap-6 cursor-pointer'>
+                <li>
+                    <a href="#">Plans</a>
+                </li>
+                <li>
+                    <a href="#">Find Domain</a>
+                </li>
+                <li>
+                    <a href="#">Why Hoster</a>
+                </li>
+            </ul>
+
+            <div className='hidden lg:flex justify-center items-center font-lato gap-6'>
+                <a className='text-gray-400' href="#">SignIn</a>
+                <button className='rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white'>Join Waitlist</button>
+            </div>
+            <div className='lg:hidden'>
                 <FaBars className='text-2xl' />
             </div>
         </div>
