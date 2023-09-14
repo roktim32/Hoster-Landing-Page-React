009b4a365a86657d402409e1e77d35a1778bdecc
Roktim Kamal Senapoty <roktimsenapoty2@gmail.com>
2023-09-15 01:57:35 +0530

    Added mobile responsive:justify-between

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar"
 function App() {
   return (
     <>
-      <div className="bg-amber-50 min-h-screen px-6 py-3 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-[120px] lg:py-10">
+      <div className="bg-amber-50 min-h-screen justify-between px-6 py-3 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-[120px] lg:py-10">
         <Navbar />
         <Body />
         <Footer />
